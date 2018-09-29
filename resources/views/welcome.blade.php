0cbd0d953f41453f914c7bf2b576894f851c1076
gicocalvento <gico.calvento2009@gmail.com>
2018-09-29 23:17:37 +0800

    add profile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laradock wtf!
+                    Laradock on Heroku!
                 </div>
 
                 <div class="links">
